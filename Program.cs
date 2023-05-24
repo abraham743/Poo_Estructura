8fa26cd304e9b062d8b53ccc0cda802730e73254
abraham743 <133922900+abraham743@users.noreply.github.com>
2023-05-24 17:36:02 -0500

    https://github.com/abraham743/Poo_Estructura.git

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Poo_Escturura.Entities;
+using Poo_Escturura.Services;
+using System;
+
+namespace Poo_Escturura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AlumnoServices servicio = new AlumnoServices();
+
+            Alumno alumno1 = servicio.Agregar();
+
+            servicio.Imprmr(alumno1);
+
+            Console.WriteLine("La calificacion es"+servicio.ValidarProm(alumno1.Promedio));
+
+        }
+    }
+}
---
+++ Services/AlumnoServices.cs
@@ -0,0 +1,112 @@
+﻿using Poo_Escturura.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Escturura.Services
+{
+    public class AlumnoServices
+    {
+        //el tipo de dato que esta a lado del "Publico" se llama"Respons" y en este caso los parametros se le llama "reques"
+        public Alumno Agregar()
+        {
+            try
+            {
+                Alumno res = new Alumno()
+                {
+                    id = 1,
+                    Nombre = "Abraham",
+                    Apellido = "Guzman",
+                    FechaIngreso = DateTime.Now,
+                    Grado = 3,
+                    Promedio = 7.5
+                };
+                return res;
+
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Hay un error"+ex.Message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+            
+        }
+        
+        public void Imprmr(Alumno request)
+        {
+            try
+            {
+                Console.WriteLine("El nombre es: " + request.Nombre);
+                Console.WriteLine("El nombre es: " + request.Apellido);
+                Console.WriteLine("El nombre es: " + request.FechaIngreso);
+                Console.WriteLine("El nombre es: " + request.Grado);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Succedio un error" + ex.Message);
+            }
+        }
+
+        public bool ValidarProm(double num1)
+        {
+            bool validar=false;
+            if (num1>7)
+            {
+                validar=true;
+            }
+            return validar;
+        }
+
+    }
+}
